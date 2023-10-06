f035d2f09ac97949116f2135ec6faca278765c6e
maldron <bagjinhyeong640@gmail.com>
2023-10-06 12:56:38 +0900

    시작

---
+++ first.R
@@ -0,0 +1,29 @@
+str1 <- "a"
+#변수에 문자 넣을 때는 큰 따옴표
+str1
+
+#ctrl+enter
+#문자 변수에는 하나의 문자,
+#여러개의 문자로 구성된 단어,
+#여러 개의 단어로 구성된 문장을 넣을 수 있다.
+#띄어쓰기, 특수문자도 가능
+
+str2 <- "string"
+str2
+str3 <- "Hello world"
+str3
+#str4 <- c("a", "b", "c")
+#str4
+str5 <- c("joohee", "Yura", "Han")
+str5
+
+#함수이름, 괄호로 구성
+#평균 mean()
+x <- c(1,2,3,4,5)
+mean(x)
+
+#최댓값 max(), 최솟값 min(), 중앙값 median()
+max(x)
+min(x)
+median(x)
+sd(x) # 표준편차 데이터 퍼져있는 정도
